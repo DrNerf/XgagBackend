1c9bee881e2186bfa9347045d5b8c892ef1d4c7d
Svetoslav Todorov <svetoslav.todorov@mentormate.com>
2017-10-09 15:01:12 +0300

    Posts server added.

---
+++ DAL/Extensions/DbContextExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL
+{
+    public static class DbContextExtensions
+    {
+        public static void AddXgagDbContext(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<XgagDbContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString("XgagDbContext")));
+        }
+    }
+}
--- PostsServer/Startup.cs
+++ PostsServer/Startup.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using DAL;
-using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Swagger;
-using AutoMapper;
+using DAL;
 
-namespace IdentityServer
+namespace PostsServer
 {
     public class Startup
     {
-        public Startup(
-            IHostingEnvironment env)
+        public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -37,14 +28,11 @@ namespace IdentityServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddAutoMapper(typeof(MappingProfile));
             services.AddXgagDbContext(Configuration);
-
-            services.AddTransient<ICryptoService>(sp => new CryptoService());
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "Xgag Identity API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "Xgag Posts API", Version = "v1" });
             });
         }
 
@@ -58,10 +46,10 @@ namespace IdentityServer
 
             app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            // Enable middle ware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Xgag Identity API");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Xgag Posts API");
             });
 
             app.UseMvc();
