36ca59ab0f23ff9ee71980e862fc3a2dc3f40e82
Svetoslav Todorov <svetoslav.todorov@mentormate.com>
2017-10-24 15:26:42 +0300

    Images route bugfix.

--- PostsServer/MappingProfile.cs
+++ PostsServer/MappingProfile.cs
@@ -14,7 +14,7 @@ namespace PostsServer
             CreateMap<Post, PostModel>()
                 .ForMember(
                     m => m.ImageUrl,
-                    m => m.MapFrom(p => $"{PostsServerAddress}/api/Posts/{p.ImageImageId}.jpg"))
+                    m => m.MapFrom(p => $"{PostsServerAddress}/api/Images/{p.ImageImageId}.jpg"))
                 .ForMember(m => m.DateCreatedTicks, m => m.MapFrom(p => p.DateCreated.Ticks))
                 .ForMember(m => m.IsYoutubePost, m => m.MapFrom(p => p.ImageImage == null))
                 .ForMember(m => m.Score, m => m.MapFrom(p => p.Votes.Sum(v => v.Type)));
